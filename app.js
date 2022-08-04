93704bbd06e0a3ee0ac909016503667bc622f15c
omiras <oscar.miras@gmail.com>
2022-08-04 08:17:07 +0200

    arreglado acceso a la página about us

--- app.js
+++ app.js
@@ -11,6 +11,8 @@ const { application } = require('express');
 app.use(express.static(path.join(__dirname, 'css')));
 app.use(express.static(path.join(__dirname, 'js')));
 app.use(express.static(path.join(__dirname, 'images')));
+app.use(express.static(path.join(__dirname, 'public')));
+
 
 
 app.use(express.urlencoded({ extended: false }));
